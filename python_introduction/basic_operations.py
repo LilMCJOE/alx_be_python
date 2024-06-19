1b702f8d1c098946b2c17ba6e3c59270186009fa
LILMCJOE <songajobs@gmail.com>
2024-06-18 18:18:21 -0700

    Basic Operation File

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+# Addition occurs here
+addition_result = number1 + number2
+# Subtraction occurs here
+subtraction_result = number1 - number2
+# Multiplication occurs here
+multiplication_result = number1 * number2
+# Results for Addition
+print(f"Addition of {number1} and {number2} is {addition_result}")
+# Results for substraction
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+# Results for Multiplication
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
